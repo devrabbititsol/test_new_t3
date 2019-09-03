668629eceb6841155c5d5c69f20e44f3afffb921
root <santhosh.annam@devrabbit.com>
2019-09-03 11:58:38 +0530

    commit

---
+++ src/test/java/api/test/api_module/apipageclasses/GetDataset.java
@@ -0,0 +1,65 @@
+package api.test.api_module.apipageclasses; 
+
+public class GetDataset{
+
+public static final int datasetLength = 1; 
+
+public static String projectName = "test";
+
+ public static String primaryInfo  = "{\"module_description\":\"desc\","
+ + "\"docker_files_directory_url\":\"https://smartqe.io:443/pdownload/DockerFiles\","
+ + "\"mobile_platform\":null,"
+ + "\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\","
+ + "\"repository_commit_message\":\"commit\","
+ + "\"browser_type\":null,"
+ + "\"client_timezone_id\":\"Asia/Calcutta\","
+ + "\"docker_files_directory\":\"/opt/tomcat9/webapps/pdownload/DockerFiles\","
+ + "\"project_url\":\"https://jsonplaceholder.typicode.com/\","
+ + "\"jenkins_username\":\"devrabbit\","
+ + "\"testcase_id\":301,"
+ + "\"project_name\":\"test\","
+ + "\"testset_name\":null,"
+ + "\"is_web\":false,"
+ + "\"jenkins_password\":\"rabbit\","
+ + "\"testcase_overwrite\":true,"
+ + "\"docker_file_name\":\"DockerFile_99_SE_TC_301\","
+ + "\"project_id\":434,"
+ + "\"framework_path\":\"/home/devrabbit/SmartQE/SmartQE-Users/99/Selenium/test\","
+ + "\"sub_module_id\":0,"
+ + "\"repository_url\":\"RaiQ0t7ecklJvfjlm04smHdEfM6bO1va8nkXLodLXP0ZLC+DQNJWzLHdGh8pZjiNA1xC1klvgxuYRi2lz2aXs2zjjuoppsbr5lhTvcOYqx3vFCHBoeQuYEa9TKRB+6Xx\","
+ + "\"sub_module_description\":null,"
+ + "\"testcase_name\":\"TC\","
+ + "\"executed_timestamp\":1567492115743,"
+ + "\"is_execute\":false,"
+ + "\"file_name\":null,"
+ + "\"aes_secret_key\":\"_SmartQE_SmartQE\","
+ + "\"testset_id\":0,"
+ + "\"jenkins_token\":\"114de69b453efd3524fce8756b1ed0e69c\","
+ + "\"is_generate\":false,"
+ + "\"jenkins_url\":\"http://192.168.1.141:8080/\","
+ + "\"project_description\":\"desc\","
+ + "\"module_id\":723,"
+ + "\"user_id\":99,"
+ + "\"bundle_id\":null,"
+ + "\"sub_module_name\":null,"
+ + "\"device_os_version\":null,"
+ + "\"execution_environment\":\"jenkins\","
+ + "\"module_name\":\"API_module\","
+ + "\"executed_user_id\":99}";
+
+
+			/*--------Default Dataset-----------*/
+
+public static final String datasetHeader1 = "TC-Default";
+
+public static final String datasetResources1 = "users";
+public static final int requestType1 = 1;
+public static final int bodyType1 = -1;
+
+ public static String urlParams1  = "[]";
+
+ public static String headers1  = "[]";
+
+ public static String body1  = "{}";
+
+}
---
+++ src/test/java/api/test/api_module/apitestclasses/TC.java
@@ -0,0 +1,65 @@
+package api.test.api_module.apitestclasses;
+
+import com.restassured.services.*;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;import com.utilities.BaseClass;
+import com.utilities.ConfigFilesUtility;
+import com.configurations.*;
+import api.test.api_module.apipageclasses.GetDataset;
+public class TC extends BaseClass {
+	ExtentReports reports;
+	ExtentTest test;
+	ITestResult result;
+	private Logger logger;
+	private ConfigFilesUtility configFileObj;
+	String projectPath = System.getProperty("user.dir");
+	String reportsPath = projectPath + "/APIReports/" + ReportPaths.reportPathName;
+
+	public TC() throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+		logger = Logger.getLogger(TC.class);
+		configFileObj= new ConfigFilesUtility();
+		configFileObj.loadPropertyFile("apiconfig.properties");
+	}
+
+	@Test(priority = 1)
+	public void doRequest() throws Exception { 
+
+		String primaryInfo = GetDataset.primaryInfo;
+		Constants.PRIMARY_INFO = primaryInfo;
+		reports = ExtentConfigurations.getExtentInstance(reportsPath, projectPath ,GetDataset.projectName);
+		test = reports.startTest("Verifying TC API");
+		int datasetLength = GetDataset.datasetLength; 
+		for(int i = 1; i <= datasetLength; i++) {
+
+			boolean isExecutionFlag = true;			
+		/*--------------------- GET --------------------- */
+
+			if(isExecutionFlag) {
+ 			 	Object object = (Object) new GetDataset();
+				Class<?> c = object.getClass();
+				String urlParams = (String) c.getField("urlParams" + i).get(object);
+				if(urlParams == null) return;
+				String headers = (String) c.getField("headers" + i).get(object);
+				String body = (String) c.getField("body" + i).get(object);
+				String datasetHeader = (String) c.getField("datasetHeader" + i).get(object);
+				String datasetResources = (String) c.getField("datasetResources" + i).get(object);
+				int reqType = (Integer) c.getField("requestType" + i).get(object);
+				int bodyType = (Integer) c.getField("bodyType" + i).get(object);
+			 	Constants.testName = Constants.testName + datasetHeader;
+				String response = APIService.callRequest(primaryInfo, urlParams, headers, reqType, bodyType, body, datasetHeader, datasetResources, test, logger);
+			}
+		}
+	}
+
+	@AfterClass
+	public void tearDown() throws Exception {
+		reports.endTest(test);
+		reports.flush();
+	}
+}
